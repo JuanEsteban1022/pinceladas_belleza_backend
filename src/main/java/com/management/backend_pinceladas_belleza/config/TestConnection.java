7652c1d9ed7d5e529e155629801844c1bf421fbd
Juan Esteban <juanesteban1022@gmail.com>
2025-11-06 16:00:29 -0500

    deply render

---
+++ src/main/java/com/management/backend_pinceladas_belleza/config/TestConnection.java
@@ -0,0 +1,24 @@
+package com.management.backend_pinceladas_belleza.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestConnection implements CommandLineRunner {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            String result = jdbcTemplate.queryForObject("SELECT version();", String.class);
+            System.out.println("✅ Conexión exitosa a PostgreSQL en Neon:");
+            System.out.println(result);
+        } catch (Exception e) {
+            System.err.println("❌ Error de conexión: " + e.getMessage());
+        }
+    }
+}
